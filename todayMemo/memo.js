9082755df35cdfb1629c73baf87b7e3a68f046f6
chooing <chumil7432@gmail.com>
2022-05-28 00:34:47 +0900

    전체 삭제 기능 추가

--- todayMemo/memo.js
+++ todayMemo/memo.js
@@ -1,16 +1,25 @@
 const writeTitle = document.querySelector('.title-input');
 const writeTxt = document.querySelector('.txt-input');
 const uploadBtn = document.querySelector('.btn-upload');
+const allDeletedBtn = document.querySelector('.all-delete-btn');
 const showUl = document.querySelector('.memo-list');
 const memes=[];
+const MEMO_LIST = 'memoList';
 
-if(localStorage.getItem('memoList')){ //localStorage에 memo가 있으면 첫 화면 작성
-    const nowMemoList = localStorage.getItem('memoList');
+if(localStorage.getItem(MEMO_LIST)){ //localStorage에 memo가 있으면 첫 화면 작성
+    const nowMemoList = localStorage.getItem(MEMO_LIST);
     memes.push(...JSON.parse(nowMemoList));
     memes.forEach(memo => {
         memoMaking(memo);
     });
 }
+allDeletedBtn.addEventListener('click',()=>{// all memo delete btn
+    if(confirm('정말로 모든 기록을 삭제하실 건가요?😱')){
+        localStorage.clear(MEMO_LIST);
+        showUl.innerHTML='';
+        alert('모든 추억이 사라집니다.😥');
+    }
+});
 
 uploadBtn.addEventListener('click',(e)=>{ // click upload btn => upload new memo 
     const userEmoji = document.querySelector('input[name="emoji"]:checked').value;
@@ -22,21 +31,25 @@ uploadBtn.addEventListener('click',(e)=>{ // click upload btn => upload new memo
             emoji:userEmoji,
         }
         memes.push(newMemo);
-        localStorage.setItem('memoList',JSON.stringify(memes));
+        localStorage.setItem(MEMO_LIST,JSON.stringify(memes));
         memoMaking(newMemo);
     }else{
         alert('텍스트를 입력해주세요.');
     }
 });
 
-showUl.addEventListener('click',(e)=>{ // delete btn
+showUl.addEventListener('click',(e)=>{ // memo delete btn
     if(e.target.classList.contains('btn-delete')){
-        const nowMemoList = localStorage.getItem('memoList');
+        const nowMemoList = localStorage.getItem(MEMO_LIST);
         const arrMemo = JSON.parse(nowMemoList);
         const parentLi = e.target.parentElement;
 
-        parentLi.remove();
-        localStorage.setItem('memoList',JSON.stringify(arrMemo.filter(x => x.id != parentLi.id)));
+        
+        if(confirm('해당 기록을 삭제합니다.')){
+            parentLi.remove();
+            localStorage.setItem(MEMO_LIST,JSON.stringify(arrMemo.filter(x => x.id != parentLi.id)));
+            alert('삭제되었습니다.');
+        }
     }
 });
 
